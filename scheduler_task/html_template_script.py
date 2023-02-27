1b8410859d80d4f1b802a563aefe697dd436651b
Tran Phuong Tung <phuongtung0801@ip-172-31-18-64.ap-southeast-2.compute.internal>
2023-02-27 12:12:54 +0000

    asdasd

--- scheduler_task/html_template_script.py
+++ scheduler_task/html_template_script.py
@@ -2,84 +2,62 @@ import frappe
 from datetime import date
 
 @frappe.whitelist()
-def site_power_custom(dateFrom, dateTo, site_name):
-
-
-    # #######
-    powerMonthlyArr  = []
-    sum = 0
-    # dateStringStart = datetime.datetime(year, month, day, 00, 00, 00)
-    # dateStringEnd = datetime.datetime(year, month, day, 23, 59, 59)
-    queryString = f"""select site_name, power_per_hour, `from`, `to` from `tabSite Power Per Hour`  where site_name = "{site_name}" and `from` > "{dateFrom}" and `to` < "{dateTo}" order by `from` asc"""
+def site_power_custom(dateFrom, dateTo):
+    queryString = f"""SELECT
+            site.name,
+            site.site_design_power,
+            site.site_real_power,
+            sum(power.power_per_hour) as total_power,
+            task.reduction_power
+        FROM `tabSite Power Per Hour` AS power 
+            INNER JOIN `tabSite` AS site ON site.name = power.site_name
+            LEFT JOIN 
+            (
+                SELECT
+                    site,
+                    reduction_power
+                FROM `tabTask`
+                WHERE
+                    (`tabTask`.exp_start_date between "{dateFrom}" AND "{dateTo}")
+                GROUP BY site
+            ) AS task ON power.site_name = task.site
+        WHERE
+             (power.from between "{dateFrom}" AND "{dateTo}")
+        GROUP BY power.site_name
+                    """
     # return queryString
     docArr = frappe.db.sql(queryString)
+    res = []
     for element in docArr:
-        sum = sum + element[1]
-        jsonObjDay = {
-            "site_name": element[0],
-            "power": element[1],
-            "from": element[2],
-            "to": element[3]
-        }
-        powerMonthlyArr.append(jsonObjDay)
-    response = {
-        "length": len(powerMonthlyArr),
-        "powerSum": sum,
-        "body": powerMonthlyArr
-    }
-    return response 
+        res.append({
+           "name": element[0],
+           "site_design_power": element[1],
+           "site_real_power": element[2],
+           "total_power": element[3],
+           "reduction_power": element[4]
+        })
+    return res 
+
+
+
+
 
 
 @frappe.whitelist()
-def site_power_detail(dateFrom, dateTo, site_name):
+def power_template():
 
-    # #######
-    powerMonthlyArr  = []
-    sum = 0
-    queryString = f"""select site_name, power_per_hour, `from`, `to` from `tabSite Power Per Hour`  where site_name = "{site_name}" and `from` > "{dateFrom}" and `to` < "{dateTo}" order by `from` asc"""
-    docArr = frappe.db.sql(queryString)
-    for element in docArr:
-        sum = sum + element[1]
-        jsonObjDay = {
-            "site_name": element[0],
-            "power": element[1],
-            "from": element[2],
-            "to": element[3]
-        }
-        powerMonthlyArr.append(jsonObjDay)
-    response = {
-        "length": len(powerMonthlyArr),
-        "powerSum": sum,
-        "body": powerMonthlyArr
-    }
-    return response 
+  #today date time
+  today = date.today()
+  todayStart = str(today) + " 00:00:00"
+  todayEnd = str(today) + " 23:59:59"
+  
+  site_power_list = site_power_custom(todayStart, todayEnd)
+  
+  return site_power_list
 
 
-queryString = f"""select site_label from `tabSite`"""
-siteList = frappe.db.sql(queryString)
-siteListParsed = []
-i = 0
-while i < len(siteList):
-    siteListParsed.append(siteList[i][0])
-    i = i + 1
-#today date time
 today = date.today()
-todayStart = str(today) + " 00:00:00"
-todayEnd = str(today) + " 23:59:59"
-
-####site power details####
-powerMonthlyArr = []
-for site in siteListParsed:
-        # return {"dateFrom":"2023-02-07 00:00:00","dateTo":"2023-02-07 18:00:00","site_name": {site}}
-        site_data = site_power_detail(todayStart,todayEnd,site)
-        for element in site_data["body"]:
-            powerMonthlyArr.append(element)
-####site power total of each site list######
-powerSum = []
-for site in siteListParsed:
-    site_test = site_power_custom(todayStart, todayEnd, site)
-    powerSum.append({"site_name": site, "power_total": site_test["powerSum"], "date": today})
-
+site_power_list = power_template()
 
 html_template = f"""
 <!DOCTYPE html>
@@ -95,14 +73,23 @@ h1, h2, h3, h4, h5, h6 {{
   font-family: "Playfair Display";
   letter-spacing: 5px;
 }}
-th, td {{padding-top: 10px;padding-bottom: 20px;padding-left: 30px;padding-right: 40px;}}
+table {{
+    padding: 10px;
+  }}
+  td, th {{
+    padding: 10px;
+    text-align: center;
+  }}
+  .no-center {{
+    text-align: left; /* align the text to the left to exclude it from the center align */
+  }}
 </style>
 </head>
 <body>
 <!-- Header -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:500px" id="home">
   <div class="w3-display-bottomleft w3-padding-large w3-opacity">
-    <h1 class="w3-xxlarge">REPORT</h1>
+    <h1 class="w3-xxlarge no-center">REPORT</h1>
   </div>
 </header>
 
@@ -113,8 +100,8 @@ th, td {{padding-top: 10px;padding-bottom: 20px;padding-left: 30px;padding-right
   <div class="w3-row w3-padding-64" id="about">
     <div class="w3-col m6 w3-padding-large">
       <h1 class="w3-center">Site Power Daily</h1><br>
-      <h5 class="w3-center">{today}</h5>
-      <p class="w3-large">This email was sent automatically by PLINK Smart Technology Joint Stock Company. Below is the daily total sites power report.</p>
+      <h5 class="w3-center">Report is taken in {today}</h5>
+      <p class="w3-large no-center">This email was sent automatically by PLINK Smart Technology Joint Stock Company. Below is the daily total sites power report.</p>
     </div>
   </div>
   
@@ -124,11 +111,30 @@ html_template += """
   <div class="w3-row w3-padding-64" id="menu">
     <div class="w3-col l6 w3-padding-large">
 """
-html_template += "<table><thead><tr><th colspan=\"5\">Site Name</th><th>Total Power (kWh)</th></tr></thead><tbody>"
-for data in powerSum:
-  html_template += "<tr><td colspan=\"5\">{site_name}</td><td>{power}</td></tr>".format(
-            site_name=data['site_name'],
-            power=data['power_total'] / 1000
+html_template += """<table>
+                      <thead><tr>
+                        <th colspan=\"5\">Site Name</th>
+                        <th colspan=\"5\">Site Design Power</th>
+                        <th colspan=\"5\">Total Power (kWh)</th>
+                        <th colspan=\"5\">Total Income (VND)</th>
+                        <th colspan=\"5\">Average Run Time</th>
+                        <th colspan=\"5\">Reduction Power</th>
+                      </tr></thead><tbody>"""
+for data in site_power_list:
+  html_template += """<tr>
+                      <td colspan=\"5\">{site_name}</td>
+                      <td colspan=\"5\">{site_design_power}</td>
+                      <td colspan=\"5\">{power}</td>
+                      <td colspan=\"5\">{total_income}</td>
+                      <td colspan=\"5\">{avg_run_time}</td>
+                      <td colspan=\"5\">{reduction_power}</td>
+                      </tr>""".format(
+            site_name=data['name'],
+            power=data['total_power'] / 1000,
+            site_design_power=data['site_design_power'],
+            total_income = data['total_power'] * 1938 / 1000,
+            avg_run_time = format(((data['total_power'] / 1000 ) / data['site_design_power']), '.4f'),
+            reduction_power=data['reduction_power']
         )
 html_template += "</tbody></table>"
 html_template += """
--- scheduler_task/tasks.py
+++ scheduler_task/tasks.py
@@ -17,126 +17,107 @@ sys.path.append(parent_dir_path + "/scheduler_task/scheduler_task/")
 from api_backend_gateway import *
 from html_template_script import html_template
 
-@frappe.whitelist()
-def site_power_custom(dateFrom, dateTo, site_name):
+# @frappe.whitelist()
+# def site_power_custom():
+
+#     site_name = "Sơn Thủy"
+#     dateFrom = "2023-02-01 00:00:00"
+#     dateTo = "2023-02-27 23:00:00"
+#     # #######
+#     powerMonthlyArr  = []
+#     sum = 0
+#     # dateStringStart = datetime.datetime(year, month, day, 00, 00, 00)
+#     # dateStringEnd = datetime.datetime(year, month, day, 23, 59, 59)
+#     queryString = f"""SELECT power.site_name, power.power_per_hour, power.`from`, power.`to`, site.site_design_power, task.reduction_power 
+#                     FROM `tabSite Power Per Hour` AS power 
+#                     INNER JOIN `tabSite` AS site ON site.name = power.site_name
+#                     INNER JOIN `tabTask` AS task ON task.site = power.site_name
+#                     WHERE power.site_name = "{site_name}" AND power.`from` > "{dateFrom}" AND power.`to` < "{dateTo}" 
+#                     ORDER BY power.`from` ASC
+#                     """
+#     # queryString = f"""SELECT * FROM `tabTask` AS task where task.site = "{site_name}" limit 100"""
+#     # return queryString
+#     # return queryString
+#     docArr = frappe.db.sql(queryString)
+#     for element in docArr:
+#         sum = sum + element[1]
+#         jsonObjDay = {
+#             "site_name": element[0],
+#             "power": element[1],
+#             "from": element[2],
+#             "to": element[3],
+#             "site_design_power": element[4],
+#             "reduction_power": element[5]
+#         }
+#         powerMonthlyArr.append(jsonObjDay)
+#     response = {
+#         "length": len(powerMonthlyArr),
+#         "powerSum": sum,
+#         "body": powerMonthlyArr
+#     }
+#     return response 
 
 
-    # #######
-    powerMonthlyArr  = []
-    sum = 0
-    # dateStringStart = datetime.datetime(year, month, day, 00, 00, 00)
-    # dateStringEnd = datetime.datetime(year, month, day, 23, 59, 59)
-    queryString = f"""select site_name, power_per_hour, `from`, `to` from `tabSite Power Per Hour`  where site_name = "{site_name}" and `from` > "{dateFrom}" and `to` < "{dateTo}" order by `from` asc"""
-    # return queryString
-    docArr = frappe.db.sql(queryString)
-    for element in docArr:
-        sum = sum + element[1]
-        jsonObjDay = {
-            "site_name": element[0],
-            "power": element[1],
-            "from": element[2],
-            "to": element[3]
-        }
-        powerMonthlyArr.append(jsonObjDay)
-    response = {
-        "length": len(powerMonthlyArr),
-        "powerSum": sum,
-        "body": powerMonthlyArr
-    }
-    return response 
+# @frappe.whitelist()
+# def site_power_detail(dateFrom, dateTo, site_name):
 
-
-@frappe.whitelist()
-def site_power_detail(dateFrom, dateTo, site_name):
-
-    # #######
-    powerMonthlyArr  = []
-    sum = 0
-    queryString = f"""select site_name, power_per_hour, `from`, `to` from `tabSite Power Per Hour`  where site_name = "{site_name}" and `from` > "{dateFrom}" and `to` < "{dateTo}" order by `from` asc"""
-    docArr = frappe.db.sql(queryString)
-    for element in docArr:
-        sum = sum + element[1]
-        jsonObjDay = {
-            "site_name": element[0],
-            "power": element[1],
-            "from": element[2],
-            "to": element[3]
-        }
-        powerMonthlyArr.append(jsonObjDay)
-    response = {
-        "length": len(powerMonthlyArr),
-        "powerSum": sum,
-        "body": powerMonthlyArr
-    }
-    return response 
+#     # #######
+#     powerMonthlyArr  = []
+#     sum = 0
+#     queryString = f"""select site_name, power_per_hour, `from`, `to` from `tabSite Power Per Hour`  where site_name = "{site_name}" and `from` > "{dateFrom}" and `to` < "{dateTo}" order by `from` asc"""
+#     docArr = frappe.db.sql(queryString)
+#     for element in docArr:
+#         sum = sum + element[1]
+#         jsonObjDay = {
+#             "site_name": element[0],
+#             "power": element[1],
+#             "from": element[2],
+#             "to": element[3]
+#         }
+#         powerMonthlyArr.append(jsonObjDay)
+#     response = {
+#         "length": len(powerMonthlyArr),
+#         "powerSum": sum,
+#         "body": powerMonthlyArr
+#     }
+#     return response 
 
 
 @frappe.whitelist()
 def cron():
-    #site list query
-    queryString = f"""select site_label from `tabSite`"""
-    siteList = frappe.db.sql(queryString)
-    siteListParsed = []
-    i = 0
-    while i < len(siteList):
-        siteListParsed.append(siteList[i][0])
-        i = i + 1
-    #today date time
-    today = date.today()
-    todayStart = str(today) + " 00:00:00"
-    todayEnd = str(today) + " 23:59:59"
+    # #site list query
+    # queryString = f"""select site_label from `tabSite`"""
+    # siteList = frappe.db.sql(queryString)
+    # siteListParsed = []
+    # i = 0
+    # while i < len(siteList):
+    #     siteListParsed.append(siteList[i][0])
+    #     i = i + 1
+    # #today date time
+    # today = date.today()
+    # todayStart = str(today) + " 00:00:00"
+    # todayEnd = str(today) + " 23:59:59"
 
-    ####site power details####
-    powerMonthlyArr = []
-    for site in siteListParsed:
-            # return {"dateFrom":"2023-02-07 00:00:00","dateTo":"2023-02-07 18:00:00","site_name": {site}}
-            site_data = site_power_detail(todayStart,todayEnd,site)
-            for element in site_data["body"]:
-                powerMonthlyArr.append(element)
-    ####site power total of each site list######
-    powerSum = []
-    for site in siteListParsed:
-        site_test = site_power_custom(todayStart, todayEnd, site)
-        powerSum.append({"site_name": site, "power_total": site_test["powerSum"], "date": today})
+    # ####site power details####
+    # powerMonthlyArr = []
+    # for site in siteListParsed:
+    #         # return {"dateFrom":"2023-02-07 00:00:00","dateTo":"2023-02-07 18:00:00","site_name": {site}}
+    #         site_data = site_power_detail(todayStart,todayEnd,site)
+    #         for element in site_data["body"]:
+    #             powerMonthlyArr.append(element)
+    # ####site power total of each site list######
+    # powerSum = []
+    # for site in siteListParsed:
+    #     site_test = site_power_custom(todayStart, todayEnd, site)
+    #     powerSum.append({"site_name": site, "power_total": site_test["powerSum"], "date": today})
   
 
-    today = datetime.date.today()
-
-    html = """
-            
-            <body>"""
-    html += f"""<h1>Plink Site's Power Daily Report {today}</h1>"""
-    html += "<br>"
-    html += "<br>"
-    html += "<br>"
-    html += "<table><thead><tr><th colspan=\"5\">Site Name</th><th>Total Power (kWh)</th></tr></thead><tbody>"
-    for data in powerSum:
-        html += "<tr><td colspan=\"5\">{site_name}</td><td>{power}</td></tr>".format(
-            site_name=data['site_name'],
-            power=data['power_total'] / 1000
-        )
-    html += "</tbody></table>"
-
-    html += "</body>"
-    
-    text_file = open('/home/phuongtung0801/frappe-bench-prod/apps/scheduler_task/scheduler_task/indextung3.html','w')
-    text_file.writelines(html_template)
-    text_file.close()
-    
-    html2 = """
-    <html>
-    <body>
-    <p><strong>This text is bold.</strong></p>
-    <p><span style="font-size: 16px;">This text is size 16.</span></p>
-    <p><span style="color: red;">This text is red.</span></p>
-    <br>
-    </body>
-    </html>
-            """
+    # today = datetime.date.today()
 
     try:
         frappe.sendmail(
-            recipients=["phuongtung0801+23@gmail.com", "phuongtung.tran0801+23@gmail.com"],
+            # recipients=["phuongtung.tran0801+23@gmail.com", "luonghuuphuloc@gmail.com", "baonguyen2409@gmail.com"],
+            recipients=["phuongtung.tran0801+23@gmail.com", "luonghuuphuloc@gmail.com"],
             subject="Plink Sites's Power Daily Report",
             message=html_template,
             delayed= False,
